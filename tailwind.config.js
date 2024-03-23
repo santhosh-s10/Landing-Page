50993241aeb4fff7778a8a9e304fd5f4ac4b4741
santhoshpandy <1012santhosh@gmail.com>
2024-03-23 15:43:36 +0530

    Landing-Page Pushed

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens:{
+      'tablet': '640px',
+
+      'laptop': '1024px',
+
+      'desktop': '1280px',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
